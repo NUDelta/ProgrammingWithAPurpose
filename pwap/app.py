c4fa8aa365b4e47c5440e94e50e05f1dd0a30528
phouse512 <philiphouse2015@u.northwestern.edu>
2014-10-31 01:45:36 -0500

    dynamic learner select elements

--- pwap/app.py
+++ pwap/app.py
@@ -19,16 +19,6 @@ app.config.from_object(config)
 db = SQLAlchemy(app)
 db.model = Base
 
-
-@app.route('/welcome', methods=['GET'])
-def welcome():	
-	return render_template('welcome.html')
-
-@app.route('/learnerEdit', methods=['GET'])
-def learnerEdit():
-	return render_template('learnerEdit.html')
-
-
 @app.route('/learner/edit/<element_id>', methods=['GET'])
 def learnerEditElement(element_id):
 	element = db.session.query(Element).filter_by(id=element_id).first()
@@ -40,9 +30,10 @@ def preview():
     html = request.args.get('html', '')
     return render_template('preview.html', css=css, html=html)
 
-@app.route('/learnerSelect', methods=['GET'])
-def learnerSelect():
-	return render_template('learnerSelect.html')
+@app.route('/learner/select', methods=['GET'])
+def select():
+	elements = db.session.query(Element)
+	return render_template('learner_select.html', elements=elements)
 
 @app.route('/upload', methods=['GET'])
 def uploadForm():
