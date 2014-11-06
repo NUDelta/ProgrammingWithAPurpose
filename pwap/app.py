1f56f8c34e78d703f54610f51d53a0823420a0d5
Ben Rothman <bensrothman@gmail.com>
2014-11-06 12:06:53 -0600

    remove pymongo reference

--- pwap/app.py
+++ pwap/app.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask import render_template, request, jsonify, make_response, Response, flash, redirect, session, url_for, g
 from pwap import config
 from pwap.models import Base, Element, Design, CodeSnippet
-from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
 app.config.from_object(config)
-
-#mongo = PyMongo(app, config_prefix='MONGO')
 
 #lm = LoginManager()
 #lm.init_app(app)
