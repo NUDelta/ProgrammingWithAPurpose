332a29d2e9608fee354d8f13e50e56ff706f566a
Alexander <alexander@dhcp-10-101-129-99.wireless.northwestern.private>
2014-11-07 12:49:51 -0600

    Fixed boostrap js bug

--- pwap/static/js/index.js
+++ pwap/static/js/index.js
@@ -7,5 +7,8 @@ window.jQuery = window.$ = require('jquery');
 window.PWAP = {
     editor: editor,
     upload: upload,
-    compile: compile
+    compile: compile,
+    login: function(){
+    	require('bootstrap');
+    }
 };
