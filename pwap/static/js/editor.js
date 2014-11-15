53c77fffe1e66a7c83aec2cab0a1aa92b3a69104
Ben Rothman <bensrothman@gmail.com>
2014-11-14 18:16:19 -0600

    update editor a bit for color detection and faster live update

--- pwap/static/js/editor.js
+++ pwap/static/js/editor.js
@@ -13,7 +13,28 @@ module.exports = function() {
         cssEditor = ace.edit('cssEditor'),
         preview = $('#preview'),
         diff = $('#diff'),
+        componentToHex = function(c) {
+            var hex = c.toString(16);
+            return hex.length == 1 ? '0' + hex : hex;
+        },
+        rgbToHex = function(dat) {
+            return '#' + componentToHex(dat[0]) + componentToHex(dat[1]) + componentToHex(dat[2]);
+        },
         update = _.throttle(function() {
+            var hasError = false,
+                annotationLists = cssEditor.getSession().getAnnotations();
+
+            _.each(annotationLists, function(annotation) {
+                if (annotation.type === 'error') {
+                    hasError = true;
+                }
+            });
+
+            if (hasError) {
+                console.log('had error');
+                return;
+            }
+
             $.post('/evaluate', {
                 opts: JSON.stringify({
                     css: cssEditor.getValue(),
@@ -31,17 +52,15 @@ module.exports = function() {
                     diff.attr('src', data.getImageDataUrl());
                 });
             });
-        }, 3000, { 'trailing': true }),
+        }, 2000, { leading: true, trailing: true }),
         img = $('#mock'),
         origin = { x: img.data('xorigin'), y: img.data('yorigin') },
         width = img.data('width'),
         height = img.data('height'),
         design = img.data('design'),
         canvas = $('#mycanvas'),
+        color = $('#color'),
         ctx = canvas[0].getContext('2d');
-
-    htmlEditor.getSession().setMode('ace/mode/html');
-    cssEditor.getSession().setMode('ace/mode/css');
 
     img.load(function() {
         ctx.drawImage(img[0], -origin.x, -origin.y);
@@ -49,13 +68,21 @@ module.exports = function() {
 
     canvas.on('click', function(e) {
         var offset = canvas.offset(),
-            dat = ctx.getImageData(e.pageX - offset.left, e.pageY - offset.top, 1, 1).data;
-        $('body').css('background-color', 'rgba(' + Array.prototype.join.call(dat) + ')');
+            dat = ctx.getImageData(e.pageX - offset.left - 2, e.pageY - offset.top - 2, 1, 1).data,
+            col = rgbToHex(dat);
+        color.css('color', col).html('&nbsp;' + col + '&nbsp;');
+
+        if (dat[0] + dat[1] + dat[2] > 255) {
+            color.css('background-color', 'black');
+        } else {
+            color.css('background-color', 'white');
+        }
     });
 
-    update();
-
-    $('#htmlEditor').add('#cssEditor').on('keyup', update);
+    htmlEditor.getSession().setMode('ace/mode/html');
+    htmlEditor.getSession().on('change', update);
+    cssEditor.getSession().setMode('ace/mode/css');
+    cssEditor.getSession().on('change', update);
 
     $('#submit').on('click', function() {
         $.post('/save/codesnippet', {
@@ -64,6 +91,7 @@ module.exports = function() {
             css: cssEditor.getValue()
         }, function(res) {
             window.alert(res);
+            window.location.href = '/';
         });
     });
 };
