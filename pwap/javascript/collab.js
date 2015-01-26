818bde00957bad806da6ec847133287827f6a3c1
Ben Rothman <bensrothman@gmail.com>
2015-01-26 16:06:58 -0600

    saving game

--- pwap/javascript/collab.js
+++ pwap/javascript/collab.js
@@ -1,6 +1,60 @@
 'use strict';
 
+var draw = function(img) {
+    var offset, i,
+        canvas = $('#mockup'),
+        ctx = canvas[0].getContext('2d'),
+        isDragging = false,
+        mxi = 0,
+        myi = 0,
+        mx = 0,
+        my = 0;
+
+    ctx.canvas.width = img.width;
+    ctx.canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+
+    offset = canvas.offset();
+    ctx.strokeStyle = 'black';
+    tick();
+
+    canvas.parent().on('mousemove', function(e) {
+        mx = Math.min(e.pageX - offset.left, img.width);
+        my = Math.min(e.pageY - offset.top, img.height);
+
+        mx = Math.max(0, mx);
+        my = Math.max(0, my);
+    }).on('mousedown', function() {
+        mxi = mx;
+        myi = my;
+        isDragging = true;
+    }).on('mouseup', function() {
+        if (isDragging) {
+            isDragging = false;
+
+        }
+    });
+}
+
 module.exports = function() {
 
-	// Nothing here yet
+    var create = function(htmlStr) {
+        var frag = document.createDocumentFragment(),
+            temp = document.createElement('div');
+
+        temp.innerHTML = htmlStr;
+        while (temp.firstChild) {
+            frag.appendChild(temp.firstChild);
+        }
+        return frag;
+    };
+    $('form').on('submit', function() {
+        var newElementType = this.elements[0].value,
+            fragment = create('<a href="#" class="list-group-item">' + newElementType + '</a>'),
+            list = document.getElementById('element-list');
+        list.insertBefore(fragment, list.childNodes[0]);
+        list.removeChild(document.getElementById('element-list-empty-message'));
+    });
+
+
 };
