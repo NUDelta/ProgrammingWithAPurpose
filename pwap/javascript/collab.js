372f69453a413eef4d337121e93d8190095b6b32
Ben Rothman <bensrothman@gmail.com>
2015-01-27 21:23:07 -0600

    progress on canvas interface. still needs more work.

--- pwap/javascript/collab.js
+++ pwap/javascript/collab.js
@@ -1,40 +1,61 @@
+/* global state */
 'use strict';
 
-var draw = function(img) {
-    var offset, i,
-        canvas = $('#mockup'),
-        ctx = canvas[0].getContext('2d'),
-        isDragging = false,
-        mxi = 0,
-        myi = 0,
-        mx = 0,
-        my = 0;
+var traverseState = function(elements, cb) {
+        console.log(elements);
+        _.forEach(elements, function(element, elementClass) {
+            cb(element, elementClass);
+            traverseState(element.children, cb);
+        });
+    },
+    draw = function() {
+        var offset, i,
+            canvas = $('#mockCanvas'),
+            img = $('#mockImg'),
+            ctx = canvas[0].getContext('2d'),
+            isDragging = false,
+            mxi = 0,
+            myi = 0,
+            mx = 0,
+            my = 0,
+            scale = img.width() / img.naturalWidth,
+            tick = function() {
+                traverseState(state, function(element) {
+                    _.forEach(element.rects, function(rect) {
+                        ctx.strokeRect.apply(ctx, rect);
+                    });
+                });
 
-    ctx.canvas.width = img.width;
-    ctx.canvas.height = img.height;
-    ctx.drawImage(img, 0, 0);
+                //window.requestAnimationFrame(tick);
+            };
 
-    offset = canvas.offset();
-    ctx.strokeStyle = 'black';
-    tick();
+        ctx.canvas.width = img.width();
+        ctx.canvas.height = img.height();
+        ctx.scale(scale, scale);
 
-    canvas.parent().on('mousemove', function(e) {
-        mx = Math.min(e.pageX - offset.left, img.width);
-        my = Math.min(e.pageY - offset.top, img.height);
+        offset = canvas.offset();
+        ctx.strokeStyle = 'black';
+        tick();
 
-        mx = Math.max(0, mx);
-        my = Math.max(0, my);
-    }).on('mousedown', function() {
-        mxi = mx;
-        myi = my;
-        isDragging = true;
-    }).on('mouseup', function() {
-        if (isDragging) {
-            isDragging = false;
+        canvas.parent().on('mousemove', function(e) {
+            mx = Math.min(e.pageX - offset.left, img.width());
+            my = Math.min(e.pageY - offset.top, img.height());
 
-        }
-    });
-}
+            mx = Math.max(0, mx);
+            my = Math.max(0, my);
+        }).on('mousedown', '#mockCanvas', function(e) {
+            mxi = mx;
+            myi = my;
+            isDragging = true;
+            e.preventDefault();
+        }).on('mouseup', function(e) {
+            if (isDragging) {
+                isDragging = false;
+
+            }
+            e.preventDefault();
+        });
+    };
 
 module.exports = function() {
 
@@ -49,10 +70,10 @@ module.exports = function() {
         return frag;
     };
     $(document).ready(function() {
-    	if (!$.isEmptyObject(state)) {
-    		$('#element-list-empty-message').remove();
-    	}
-    	renderElementList(state);
+        if (!$.isEmptyObject(state)) {
+            $('#element-list-empty-message').remove();
+        }
+        renderElementList(state);
     });
     $('form').on('submit', function() {
         var newElementType = this.elements[0].value,
@@ -62,18 +83,20 @@ module.exports = function() {
         list.removeChild(document.getElementById('element-list-empty-message'));
     });
     var renderElementList = function(obj) {
-    	for (var element in obj) {
-    		renderElement(element);
-    		if (obj[element].children !== null) {
-    			renderElementList(obj[element].children);
-    		}
-    	}
+        for (var element in obj) {
+            renderElement(element);
+            if (obj[element].children !== null) {
+                renderElementList(obj[element].children);
+            }
+        }
     };
     var renderElement = function(element) {
-    	var $list = $('#element-list'),
-    		$template = $('#element-template').clone(),
-    		$listItem = $template.attr('id', element);
-    	$listItem.text(element);
-    	$list.append($listItem);
-    }
+        var $list = $('#element-list'),
+            $template = $('#element-template').clone(),
+            $listItem = $template.attr('id', element);
+        $listItem.text(element);
+        $list.append($listItem);
+    };
+
+    draw();
 };
