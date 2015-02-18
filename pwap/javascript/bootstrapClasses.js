2e1bf18e99083611d2a8c3ae92e2a1f0c83be547
phouse512 <philiphouse2015@u.northwestern.edu>
2015-02-17 19:53:45 -0600

    Phil's half

--- pwap/javascript/bootstrapClasses.js
+++ pwap/javascript/bootstrapClasses.js
@@ -30,5 +30,59 @@ module.exports = {
     '.text-success',
     '.text-info',
     'blockquote'
+  ],
+  'Forms': [
+    'fieldset',
+    'legend',
+    'label',
+    'input[type="search"]',
+    'input[type="radio"]',
+    'input[type="checkbox"]',
+    'input[type="file"]',
+    'input[type="range"]',
+    'select[multiple]',
+    'select[size]',
+    'output',
+    '.form-control',
+    '.form-group',
+    '.radio',
+    '.checkbox',
+    '.form-group-sm',
+    '.form-group-lg',
+    '.input-lg'
+  ],
+  'Alerts': [
+    '.alert',
+    '.alert-dismissable',
+    '.alert-success',
+    '.alert-info',
+    '.alert-warning',
+    '.alert-danger'
+  ],
+  'Badges': [
+    '.badge'
+  ],
+  'Breadcrumbs': [
+    '.breadcrumb'
+  ],
+  'Button Groups': [
+    'btn-group',
+    'btn-group-vertical',
+    'btn-toolbar'
+  ],
+  'Carousel': [
+    '.carousel',
+    '.carousel-inner',
+    '.carousel-control',
+    '.carousel-indicators'
+  ],
+  'Close': [
+    '.close'
+  ],
+  'Code': [
+    'code',
+    'kbd',
+    'pre',
+    'samp'
   ]
 };
